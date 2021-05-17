32c085dc2357bd6b068d916c6679caa7b2ff430d
wz2cool <wz2cool@live.cn>
2021-05-17 14:03:02 +0800

    init

---
+++ src/main/java/com/github/wz2cool/elasticsearch/query/LogicPagingQuery.java
@@ -0,0 +1,51 @@
+package com.github.wz2cool.elasticsearch.query;
+
+import com.github.wz2cool.elasticsearch.lambda.GetLongPropertyFunction;
+import com.github.wz2cool.elasticsearch.model.UpDown;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.sort.SortOrder;
+
+public class LogicPagingQuery<T> {
+
+    private final Class<T> clazz;
+    private final UpDown upDown;
+    private final SortOrder sortOrder;
+    private final GetLongPropertyFunction<T> pagingPropertyFunc;
+    private QueryBuilder queryBuilder;
+
+    private LogicPagingQuery(Class<T> clazz, GetLongPropertyFunction<T> pagingPropertyFunc, SortOrder sortOrder, UpDown upDown) {
+        this.clazz = clazz;
+        this.upDown = upDown;
+        this.sortOrder = sortOrder;
+        this.pagingPropertyFunc = pagingPropertyFunc;
+    }
+
+    public static <T> LogicPagingQuery<T> createQuery(
+            Class<T> clazz, GetLongPropertyFunction<T> pagingPropertyFunc, SortOrder sortOrder, UpDown upDown) {
+        return new LogicPagingQuery<>(clazz, pagingPropertyFunc, sortOrder, upDown);
+    }
+
+    public QueryBuilder getQueryBuilder() {
+        return queryBuilder;
+    }
+
+    public void setQueryBuilder(QueryBuilder queryBuilder) {
+        this.queryBuilder = queryBuilder;
+    }
+
+    public Class<T> getClazz() {
+        return clazz;
+    }
+
+    public UpDown getUpDown() {
+        return upDown;
+    }
+
+    public SortOrder getSortOrder() {
+        return sortOrder;
+    }
+
+    public GetLongPropertyFunction<T> getPagingPropertyFunc() {
+        return pagingPropertyFunc;
+    }
+}
