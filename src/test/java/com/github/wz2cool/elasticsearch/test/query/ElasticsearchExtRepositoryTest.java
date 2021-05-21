9ed263f50897040fc4997c15c4831f377ddc77f4
wz2cool <wz2cool@live.cn>
2021-05-21 17:38:14 +0800

    support querymode

--- src/test/java/com/github/wz2cool/elasticsearch/test/query/ElasticsearchExtRepositoryTest.java
+++ src/test/java/com/github/wz2cool/elasticsearch/test/query/ElasticsearchExtRepositoryTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = TestApplication.class)
+@SuppressWarnings("all")
 public class ElasticsearchExtRepositoryTest {
 
     @Resource
